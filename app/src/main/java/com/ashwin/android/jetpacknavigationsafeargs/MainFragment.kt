0fb63ee8eca8dad6500e6236d2e20c27dcd02cfc
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2020-04-11 19:34:16 +0530

    First commit

---
+++ app/src/main/java/com/ashwin/android/jetpacknavigationsafeargs/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.ashwin.android.jetpacknavigationsafeargs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+    lateinit var navController: NavController
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        next_button.setOnClickListener {
+            val message = message_edittext.text.toString()
+
+            val action = MainFragmentDirections.actionMainFragmentToNextFragment(message)
+
+            navController.navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/ashwin/android/jetpacknavigationsafeargs/NextFragment.kt
@@ -0,0 +1,24 @@
+package com.ashwin.android.jetpacknavigationsafeargs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_next.*
+
+class NextFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_next, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Get the safe-args (String) message
+        val args: NextFragmentArgs = NextFragmentArgs.fromBundle(arguments ?: Bundle.EMPTY)
+        val message = args.message
+
+        message_textview.text = message
+    }
+}
